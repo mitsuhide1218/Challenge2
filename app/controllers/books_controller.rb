2893a1e1129f9c1b016f37ead6fcafda880a2078
吉田　充秀 <mauser_c96@train.ocn.ne.jp>
2020-02-12 16:58:40 +0900

    [Finish]4-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,53 @@
+class BooksController < ApplicationController
+	def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def new
+  	
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	book = Book.find(params[:id])
+  	if book.update(book_params)
+      flash[:notice2] = "Book was successfully updated."
+    	redirect_to book_path(book.id)
+    else
+    @book = book
+    render action: :edit
+    end
+  end
+
+  def create
+  	book = Book.new(book_params)
+    if book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    else
+      @book = book
+      @books = Book.all
+       render action: :index
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+
+end
